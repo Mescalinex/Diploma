f4ef8b98b2d0912dbefde5ff13611707ce7965e0
Mikhail <mikhailmashchenko1@gmail.com>
2019-03-26 20:24:32 +0300

    lambda

--- yahoo_parser.py
+++ yahoo_parser.py
@@ -9,9 +9,10 @@ def income_parse(company_list):
     URL = 'https://www.marketwatch.com/investing/stock/fslr/financials/income/quarter'
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
-    financials = soup.find('table', {'class':'crDataTable'}).tbody
-    rows = financials.find_all('tr')
+#    financials = soup.find('table', {'class':'crDataTable'}).tbody
+    table = soup.find_all(lambda tag: tag.name=='table')
+#    rows = financials.find_all('tr')
 #    columns = for v in rows[0].find_all('th')
-    print(financials)
+    print(table)
     
 income_parse(company_list)
