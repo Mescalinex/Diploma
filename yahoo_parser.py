fd447ac309a4770c7c65c23e1c4524b3609993c1
Mikhail <mikhailmashchenko1@gmail.com>
2019-03-27 13:51:24 +0300

    straggling

--- yahoo_parser.py
+++ yahoo_parser.py
@@ -8,11 +8,13 @@ company_list = df['Symbol']
 def income_parse(company_list):
     URL = 'https://www.marketwatch.com/investing/stock/fslr/financials/income/quarter'
     response = requests.get(URL)
-    soup = BeautifulSoup(response.text, 'html.parser')
-#    financials = soup.find('table', {'class':'crDataTable'}).tbody
-    table = soup.find_all(lambda tag: tag.name=='table')
-#    rows = financials.find_all('tr')
-#    columns = for v in rows[0].find_all('th')
-    print(table)
+    soup = BeautifulSoup(response.text, 'lxml')
+    financials = soup.find_all('table', class_='crDataTable')
+    rows = []
+    for lines in financials:
+        values = lines.find_all('td')
+        rows.append(values)
+    
+    print(financials)
     
 income_parse(company_list)
